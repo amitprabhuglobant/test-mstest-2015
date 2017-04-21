cbffcfea1c3348ca257b3134c2c5b8379b44c0f7
amitprabhuglobant <amit.prabhu@globant.com>
2017-04-21 17:54:31 +0530

    first nunit

---
+++ coresample/src/coresample/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coresample
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestClass1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ mstestsample/src/mstestsample/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mstestsample
+{
+    [TestClass]
+    public class TestClass
+    {
+        [TestMethod]
+        public void TestMethodPassing()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethodFailing()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
